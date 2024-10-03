eb76e38c59aee77db09971aa21c02b6798648b31
Lentucky <virballen@gmail.com>
2024-10-03 08:01:51 +0800

    Updated Dashboard

--- index.php
+++ index.php
@@ -8,6 +8,70 @@
                 <img src="img/image.png" alt="">
             </form>
         </div>
+
+        <div class="card-container">
+            <div class="card">
+                <i class='bx bxs-wallet-alt'></i>
+
+                <h1>4000</h1>
+                <h2>Total Revenue</h2>
+                <h3>this month</h3>
+            </div>
+            <div class="card">
+                <i class='bx bxs-notepad'></i>
+
+                <h1>4</h1>
+                <h2>Appointments</h2>
+            </div>
+            <div>
+                <h1>Calendar here</h1>
+            </div>
+        </div>
+
+        <div class="table-container">
+            <h3>Upcoming Appointments</h3>
+            <table class="dash-table">
+                <tr>
+                    <td>Anna Dagos</td>
+                    <td>Pasta</td>
+                    <td>Sept 24, 2024</td>
+                    <td>11:00 AM</td>
+                    <td>30 min</td>
+                    <td><i class='bx bx-dots-horizontal-rounded'></i></td>
+                </tr>
+
+                <tr>
+                    <td>Anna Dagos</td>
+                    <td>Pasta</td>
+                    <td>Sept 24, 2024</td>
+                    <td>11:00 AM</td>
+                    <td>30 min</td>
+                    <td><i class='bx bx-dots-horizontal-rounded'></i></td>
+                </tr>
+
+                <tr>
+                    <td>Anna Dagos</td>
+                    <td>Pasta</td>
+                    <td>Sept 24, 2024</td>
+                    <td>11:00 AM</td>
+                    <td>30 min</td>
+                    <td><i class='bx bx-dots-horizontal-rounded'></i></td>
+                </tr>
+
+                <tr>
+                    <td>Anna Dagos</td>
+                    <td>Pasta</td>
+                    <td>Sept 24, 2024</td>
+                    <td>11:00 AM</td>
+                    <td>30 min</td>
+                    <td>
+                        <a href="">
+                            <i class='bx bx-dots-horizontal-rounded'></i>
+                        </a>
+                    </td>
+                </tr>
+            </table>
+        </div>
     </main>
 
 <?php include('partials/footer.php') ?>
